48392ef50945f01b6cfc4f7b97a2cbabc0005f1c
jirikuchta <jiri.kuchta@firma.seznam.cz>
2021-02-03 21:03:23 +0100

    change bundles names to ignore them in github language statistics

--- static/rollup.config.js
+++ static/rollup.config.js
@@ -2,6 +2,6 @@ import includePaths from "rollup-plugin-includepaths";
 
 export default {
 	input: ".build/app.js",
-	output: { file: "app.js" },
+	output: { file: "app.min.js" },
 	plugins: [includePaths({paths: [".build/"]})]
 }
