50e96a07b844d47a13ab52a74583630ae603ce63
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-02-18 16:32:52 +0100

    LinkParser

--- app/lib/feedicon.py
+++ app/lib/feedicon.py
@@ -1,12 +1,10 @@
 from base64 import b64decode
 from io import BytesIO
-from typing import List, Tuple, Optional
-from html.parser import HTMLParser
 
 from flask import Response
 
 from models import Subscription
-from lib.utils import ensure_abs_url, request
+from lib.utils import ensure_abs_url, request, LinkParser
 
 # 1px transparent GIF
 DEFAULT = b64decode(
@@ -29,49 +27,15 @@ def fetch(subscription: Subscription) -> Response:
                 charset = response.headers.get_content_charset("utf-8")
                 html = response.read().decode(charset)
 
-            icon_url = FaviconParser.parse(html)
+            parser = LinkParser(html)
 
-            if icon_url:
-                icon_url = ensure_abs_url(web_url, icon_url)
+            if len(parser.icons):
+                icon_url = ensure_abs_url(web_url, next(
+                    filter(lambda i: i["size"] == 16, parser.icons),
+                    parser.icons[0])["href"])
                 with request(icon_url) as res:
                     icon = res.read()
         except Exception:
             pass
 
     return Response(BytesIO(icon), mimetype="image/x-icon")
-
-
-class FaviconParser(HTMLParser):
-
-    @staticmethod
-    def parse(html: str) -> Optional[str]:
-        parser = FaviconParser()
-        parser.feed(html)
-        return parser.icon_href
-
-    def __init__(self) -> None:
-        super().__init__()
-        self._icon_href: Optional[str] = None
-
-    @property
-    def icon_href(self) -> Optional[str]:
-        return self._icon_href
-
-    def handle_starttag(
-            self, tag: str, attrs: List[Tuple[str, Optional[str]]]) -> None:
-        if self._is_icon_link(tag, attrs):
-            href, size = None, None
-            for k, v in attrs:
-                if k == "href" and v:
-                    href = v
-                if k == "sizes" and v:
-                    size = int(v.split("x")[0])
-
-            if href and (not self._icon_href or size == 16):
-                self._icon_href = href.strip()
-
-    def _is_icon_link(self, tag: str, attrs: List[Tuple[str, Optional[str]]]):
-        if tag == "link":
-            for k, v in attrs:
-                if k == "rel" and v and "icon" in v.split(" "):
-                    return True
--- app/lib/utils.py
+++ app/lib/utils.py
@@ -1,3 +1,6 @@
+from html.parser import HTMLParser
+from http.client import HTTPResponse
+from typing import TypedDict, List, Tuple, Optional
 from urllib.parse import urljoin
 from urllib import request as req
 
@@ -6,5 +9,55 @@ def ensure_abs_url(base: str, url: str) -> str:
     return urljoin(base, url)
 
 
-def request(url: str):
+def request(url: str) -> HTTPResponse:
     return req.urlopen(req.Request(url, headers={"User-agent": "RSS reader"}))
+
+
+class IconLink(TypedDict):
+    href: str
+    size: Optional[int]
+
+
+class FeedLink(TypedDict):
+    href: str
+    title: Optional[str]
+
+
+class LinkParser(HTMLParser):
+
+    def __init__(self, html: str) -> None:
+        super().__init__()
+
+        self._icons: List[IconLink] = []
+        self._feeds: List[FeedLink] = []
+
+        self.feed(html)
+
+    @property
+    def icons(self) -> List[IconLink]:
+        return self._icons
+
+    @property
+    def feeds(self) -> List[FeedLink]:
+        return self._feeds
+
+    def handle_starttag(self, tag: str,
+                        attrs: List[Tuple[str, Optional[str]]]) -> None:
+        if tag != "link":
+            return
+
+        attrs_dict = {k: v for k, v in attrs}
+        rel = attrs_dict.get("rel")
+        type = attrs_dict.get("type")
+        href = attrs_dict.get("href")
+
+        if href and rel and "icon" in rel.split(" "):
+            size = attrs_dict.get("sizes")
+            self._icons.append({
+                "href": href,
+                "size": int(size.split("x")[0]) if size else None})
+
+        if href and type in ("application/rss+xml", "application/atom+xml"):
+            self._feeds.append({
+                "href": href,
+                "title": attrs_dict.get("title")})
