ccb28fd5b8fcfe3dc1bcd39322948150eabac8bf
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-10-30 15:44:08 +0200

    hardcoded default config

--- app/lib/config.py
+++ app/lib/config.py
@@ -1,18 +1,18 @@
 from typing import TypedDict
-from os import environ
+from os import environ, urandom
 from flask import Flask
 
 
 class Config(TypedDict):
-    DEBUG: bool
-    SECRET_KEY: str
-    UPDATER_RUN_INTERVAL_SECONDS: int
-    SUBSCRIPTION_UPDATE_INTERVAL_SECONDS: int
-    PURGE_ARTICLE_AGE_DAYS: int
-    PURGE_UNREAD_ARTICLES: bool
-    SQLALCHEMY_DATABASE_URI: str
-    SQLALCHEMY_ECHO: bool
-    SQLALCHEMY_TRACK_MODIFICATIONS: bool
+    DEBUG: bool = False
+    SECRET_KEY: str = urandom(16)
+    UPDATER_RUN_INTERVAL_SECONDS: int = 5*60
+    SUBSCRIPTION_UPDATE_INTERVAL_SECONDS: int = 30*60
+    PURGE_ARTICLE_AGE_DAYS: int = 7
+    PURGE_UNREAD_ARTICLES: bool = False
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///rss_reader.sqlite"
+    SQLALCHEMY_ECHO: bool = False
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
 
 
 def init(app: Flask) -> None:
@@ -20,6 +20,7 @@ def init(app: Flask) -> None:
         value = environ.get(f"RSS_READER_{key}", environ.get(key))
 
         if value is None:
+            app.config[key] = getattr(Config, key)
             continue
 
         if key_type is bool:
